6c095832e9c1009ce3ff882342a5cd1fc748dfaf
kon2raya24 <konturaya@gmail.com>
2026-06-27 09:03:03 +0000

    docs: add JSDoc comments to maskstring.ts

--- src/maskstring.ts
+++ src/maskstring.ts
@@ -1,3 +1,10 @@
+/**
+ * maskString
+ * @param str
+ * @param visibleStart = 2
+ * @param visibleEnd = 2
+ * @param maskChar = "*"
+ */
 export function maskString(str: string, visibleStart = 2, visibleEnd = 2, maskChar = "*"): string {
   if (str.length <= visibleStart + visibleEnd) return str;
   return str.slice(0, visibleStart) + maskChar.repeat(str.length - visibleStart - visibleEnd) + str.slice(-visibleEnd);
